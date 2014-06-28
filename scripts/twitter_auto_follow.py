2230525ee52dded541b4f773d8d5debc6e8a59af
jonathan <jonathan@calazan.com>
2014-06-28 00:34:21 -0400

    Updated twitter auto-follow script.

--- scripts/twitter_auto_follow.py
+++ scripts/twitter_auto_follow.py
@@ -15,10 +15,29 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-search_results = api.search(q='#bgnow')
+search_terms = [
+    '#bgnow',
+    '#t1d',
+    '#insulin',
+]
 
-for result in search_results:
-    if not result.user.following:
-        print 'Following %s...' % result.user.screen_name
-        result.user.follow()
-        time.sleep(3)
+for search_term in search_terms:
+    print 'Searching for %s...' % search_term
+    search_results = api.search(q=search_term)
+    for result in search_results:
+        if not result.user.following:
+            print 'Following %s...' % result.user.screen_name
+            result.user.follow()
+            time.sleep(1)
+
+
+# Unfollow users not following me (yes, not nice, but gotta cleanup sometimes).
+# for f in tweepy.Cursor(api.friends).items(1200):
+#     source, target = api.show_friendship(source_screen_name=f.screen_name,
+#                                          target_screen_name='glucosetracker')
+#     print source.screen_name, target.screen_name, target.followed_by
+#
+#     if not target.followed_by:
+#         api.destroy_friendship(screen_name=source.screen_name)
+#
+#     time.sleep(20)
