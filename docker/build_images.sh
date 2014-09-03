fca64f97983d54bbe0cb2ecd17b7ce3cccc626a9
jcalazan <jcalazan@noodle.org>
2014-09-02 21:00:10 -0400

    Updated docker scripts.

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -2,15 +2,15 @@
 
 # Build 'base' image. Must be built first as all other images depend on it.
 cd base
-docker build -t jcalazan/base .
+docker build -t jcalazan/base --no-cache .
 cd ..
 
 # Build 'postgresql' image.
 cd postgresql
-docker build -t jcalazan/postgresql .
+docker build -t jcalazan/postgresql --no-cache .
 cd ..
 
 # Build 'django' image.
 cd django
-docker build -t jcalazan/django .
+docker build -t jcalazan/django --no-cache .
 cd ..
